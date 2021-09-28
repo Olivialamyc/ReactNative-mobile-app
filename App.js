fd2db02560ffeeeed24712e6a930d4b70f4bcadb
Olivialamyc <lamemily0209@gmail.com>
2021-09-28 21:49:26 +0800

    update login page and button

--- App.js
+++ App.js
@@ -1,18 +1,53 @@
 import React from 'react';
+import { setStatusBarStyle, StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Pressable } from 'react-native';
 import LoginScreen from './src/login/login.js'
 import Icon from 'react-native-ico-material-design';
+import style from './src/Login/style.js';
 
 const IconHeight = 26;
 const IconWidth = 26;
 
 
 export default class App extends React.Component {
+  state = {
+    screenText: "Press a button"
+  }
+
+  changeText = (text) =>{
+    console.log(text + 'has been pressed.')
+    this.setState({
+      screenText : text
+    })
+  }
+
   render() {
     return (
       <View>
+        <View>
+      <Text  style={{fontSize:30}}>{this.state.screenText}</Text>
       <Icon name="add-plus-button" height="40" width="40" />
-      <LoginScreen />
+      <StatusBar style='light' />
+      
+      </View>
+      <View style={styles.NavContainer}>
+        <View style={styles.NavBar}>
+        <Pressable onPress={() =>  this.changeText('Welcome')} style={styles.IconBehave}
+        android_ripple={{borderless:true, radius:50}}>
+          <Icon name="front-store" height={iconHeight} width={iconWidth} color='448aff' />
+
+        </Pressable>
+        <Pressable onPress={() =>  this.changeText('Login')} style={styles.IconBehave}
+        android_ripple={{borderless:true, radius:50}}>
+          <Icon name="user-account-box" height={iconHeight} width={iconWidth} color='448aff' />
+          
+        </Pressable>
+        <LoginScreen />
+
+        </View>
+      </View>
+
+
 
       </View>
 
@@ -23,8 +58,29 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#282b75',
+    color: '#a296ca',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  NavContainer:{
+    position:'absolute',
+    alignItems:'center',
+    bottom:20
+  },
+  NavBar:{
+    flexDirection:'row',
+    backgroundColor:'#eee',
+    width: '90%',
+    justifyContent: 'space-evenly',
+    borderRadius: 40
+
+
+  },
+
+  IconBehave:{
+    padding: 14
+
+  } 
+   
 });
--- src/Login/login.js
+++ src/Login/login.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import { StyleSheet } from "react-native";
 import styles from "./style";
 import {Keyboard, Text, View, TextInput, TouchableWithoutFeedback, Alert, KeyboardAvoidingView} from 'react-native';
 import { Button } from 'react-native-elements';
@@ -60,3 +60,51 @@ export default class LoginScreen extends Component {
     }
   }
 }
+
+const styles = StyleSheet.create({
+
+
+containerView: {
+  flex: 1,
+},
+loginScreenContainer: {
+  flex: 1,
+},
+logoText: {
+  fontSize: 40,
+  fontWeight: "800",
+  marginTop: 150,
+  marginBottom: 30,
+  textAlign: 'center',
+},
+loginFormView: {
+  flex: 1
+},
+loginFormTextInput: {
+  height: 50,
+  fontSize: 14,
+  borderRadius: 5,
+  borderWidth: 1,
+  borderColor: '#eaeaea',
+  backgroundColor: '#fafafa',
+  paddingLeft: 10,
+  marginLeft: 15,
+  marginRight: 15,
+  marginTop: 5,
+  marginBottom: 5,
+
+},
+loginButton: {
+  backgroundColor: '#3897f1',
+  borderRadius: 5,
+  height: 45,
+  marginTop: 10,
+  marginLeft: 15,
+  marginRight: 15,
+},
+fbLoginButton: {
+  height: 45,
+  marginTop: 10,
+  backgroundColor: 'transparent',
+},
+})
